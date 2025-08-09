bb9fed17b9761c91fd72430b02b8f0bf874197c7
mpaquette8 <64750501+mpaquette8@users.noreply.github.com>
2025-08-09 16:10:06 +0200

    Delete backend/utils-old directory

--- backend/utils-old/prisma.js
+++
@@ -1,9 +0,0 @@
-// backend/utils/prisma.js
-
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-});
-
-module.exports = prisma;
