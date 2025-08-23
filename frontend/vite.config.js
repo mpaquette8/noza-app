4998855fca5f0a8222de896852a0037159247058
mpaquette8 <64750501+mpaquette8@users.noreply.github.com>
2025-08-23 14:56:08 +0200

    Use Terser plugin for Vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { terser } from '@rollup/plugin-terser';
 
 export default defineConfig({
   build: {
@@ -14,8 +15,15 @@ export default defineConfig({
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]'
-      }
+      },
+      plugins: [terser({ compress: { drop_console: true, drop_debugger: true } })]
     },
-    minify: true
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   }
 });
